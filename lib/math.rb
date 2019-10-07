30711a83dcbb6472e8ea942ae75d15b3ca0e7aa7
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-07 21:56:37 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,5 +1,5 @@
 def addition(num1, num2)
-
+  num1 + num2
 end
 
 def subtraction(num1, num2)
@@ -21,3 +21,9 @@ end
 def square_root(num)
 
 end
+addition(1, 2)
+subtraction(2, 1)
+division(4,2)
+multiplication(1, 2)
+modulo(4, 1)
+square_root(5, 2)
